639d4c4157528b1a41512d96de518898e50c9b14
Geek_dev <shirkha_ug@ece.nits.ac.in>
2022-07-01 00:34:21 +0530

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
@@ -10,55 +11,56 @@ const mongoose = require("mongoose")
 mongoose.connect(process.env.DATABASE, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
-}).then(res=> console.log("Connected to db")).catch(err=>console.log(err))
+}).then(res => console.log("Connected to db")).catch(err => console.log(err))
 
 
 app.get("/", (req, res) => {
-    const code = `Welcome to ShareCode!
+  const code = `Welcome to ShareCode!
 
 Use the commands in the top right corner
 to create a new file to share with others.`
-    res.render("code-display", { code, language: "plaintext" })
+  res.render("code-display", { code, language: "plaintext" })
 })
 
-app.get("/new",(req,res) => {
-    res.render("new")
+
+app.get("/new", (req, res) => {
+  res.render("new")
 })
 
 app.post("/save", async (req, res) => {
-    const value = req.body.value
-    try {
-      const document = await Document.create({ value })
-      res.redirect(`/${document.id}`)
-    } catch (e) {
-      res.render("new", { value })
-    }
-  })
+  const value = req.body.value
+  try {
+    const document = await Document.create({ value })
+    res.redirect(`/${document.id}`)
+  } catch (e) {
+    res.render("new", { value })
+  }
+})
 
-  app.get("/:id/duplicate", async (req, res) => {
-    const id = req.params.id
-    try {
-      const document = await Document.findById(id)
-      res.render("new", { value: document.value })
-    } catch (e) {
-      res.redirect(`/${id}`)
-    }
-  })
-  
-  app.get("/:id", async (req, res) => {
-    const id = req.params.id
-    try {
-      const document = await Document.findById(id)
-  
-      res.render("code-display", { code: document.value, id })
-    } catch (e) {
-      res.redirect("/")
-    }
-  })
+app.get("/:id/duplicate", async (req, res) => {
+  const id = req.params.id
+  try {
+    const document = await Document.findById(id)
+    res.render("new", { value: document.value })
+  } catch (e) {
+    res.redirect(`/${id}`)
+  }
+})
+
+app.get("/:id", async (req, res) => {
+  const id = req.params.id
+  try {
+    const document = await Document.findById(id)
+
+    res.render("code-display", { code: document.value, id })
+  } catch (e) {
+    res.redirect("/")
+  }
+})
 
 
 
 let port = process.env.PORT || 3000
-app.listen(port, ()=>{
-    console.log(`Server running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
 })
